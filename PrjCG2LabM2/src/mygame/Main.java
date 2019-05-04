08d3b61fd175ddd0db19c8c135dcc66690d7ed27
Gabriel Yudi <g.sanefugi@gmail.com>
2019-05-04 15:58:13 -0300

    Camera
    -Configurando camera
    -Seguir o cubo

--- PrjCG2LabM2/src/mygame/Main.java
+++ PrjCG2LabM2/src/mygame/Main.java
@@ -5,9 +5,8 @@ import com.jme3.input.KeyInput;
 import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.KeyTrigger;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
-import com.jme3.scene.Geometry;
-import com.jme3.scene.shape.Box;
 import javax.swing.JOptionPane;
 import mygame.Classes.*;
 
@@ -21,6 +20,7 @@ public class Main extends SimpleApplication{
     private Mapa m;
     private int mapaAtual = 1; // Defini qual é o mapa atual que deve ser gerado
     private int auxAnimacao = 0; //Variavel auxiliar para controle de animação (0 - parado; 1,2,3,4 - Em animação e qual tipo)
+    private int auxCamera = 1; // Auxiliar camera, que tem pre-configurações
     
     public static void main(String[] args) {
         Main app = new Main();
@@ -31,10 +31,6 @@ public class Main extends SimpleApplication{
     @Override
     public void simpleInitApp() 
     {
-        controle();
-        //cam.setLocation(new Vector3f(-1, -1, 5));
-        //cam.setRotation(new Quaternion(0, 0, 0, 0));
-        
         viewPort.setBackgroundColor(ColorRGBA.DarkGray);
         
         m = new Mapa(assetManager,rootNode);
@@ -43,10 +39,14 @@ public class Main extends SimpleApplication{
         m.gerarMapa(mapaAtual);
         c.gerarCubo();
         
+        camera(auxCamera);
+        controle();
+        
     }
 
     @Override
     public void simpleUpdate(float tpf) {
+        
         if (auxAnimacao != 0) 
         {
             c.anima(auxAnimacao, tpf*3); //o Multiplo define a velocidade de animação
@@ -58,6 +58,8 @@ public class Main extends SimpleApplication{
                 auxAnimacao = 0;
                 
                 c.gerarCubo();
+                
+                camera(auxCamera);
                 
                 if(VerificarDerrota())
                 {
@@ -97,6 +99,14 @@ public class Main extends SimpleApplication{
                 if (name.equals("Down") && !keyPressed) {
                     auxAnimacao = 2;
                 }
+                if (name.equals("Camera1") && !keyPressed) {
+                    auxCamera = 1;
+                    camera(auxCamera);
+                }
+                if (name.equals("Camera2") && !keyPressed) {
+                    auxCamera = 2;
+                    camera(auxCamera);
+                }
             }
         }
     };
@@ -106,11 +116,34 @@ public class Main extends SimpleApplication{
         inputManager.addMapping("Up",  new KeyTrigger(KeyInput.KEY_I));
         inputManager.addMapping("Left",   new KeyTrigger(KeyInput.KEY_J));
         inputManager.addMapping("Down",  new KeyTrigger(KeyInput.KEY_K));
+        inputManager.addMapping("Camera1",  new KeyTrigger(KeyInput.KEY_U));
+        inputManager.addMapping("Camera2",  new KeyTrigger(KeyInput.KEY_O));
         
         inputManager.addListener(actionListener, "Right");
         inputManager.addListener(actionListener, "Left");
         inputManager.addListener(actionListener, "Up");
         inputManager.addListener(actionListener, "Down");
+        inputManager.addListener(actionListener, "Camera1");
+        inputManager.addListener(actionListener, "Camera2");
+    }
+    
+    private void camera(int i) {
+        
+        switch(i)
+        {
+            case 1:
+                cam.setLocation(new Vector3f(-4f + c.getX(), -2f + c.getY(), 7));
+                cam.lookAt(new Vector3f(c.getX(), c.getY(), 0), new Vector3f(0, 0, 0));
+                break;
+                
+            case 2:
+                cam.setLocation(new Vector3f(4f + c.getX(), 2f + c.getY(), 7));
+                cam.lookAt(new Vector3f(c.getX(), c.getY(), 0), new Vector3f(0, 0, 0));
+                break;
+                
+            default:
+                throw new Error("ERRO - Valor camera indefinido");
+        }
     }
     
     //Esta parte ira confirma se o jogador ganhou ou nao
@@ -161,6 +194,7 @@ public class Main extends SimpleApplication{
     private void renicia()
     {
         auxAnimacao = 0;
+        auxCamera = 1;
         rootNode.detachAllChildren();
         c = new Cubo(assetManager, rootNode);
         m = new Mapa(assetManager, rootNode);
